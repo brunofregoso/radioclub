8c2a1e24086a0e257a85954bdc282132ec864b17
Bruno <bruno88756@gmail.com>
2024-09-28 22:18:34 -0400

    sessions fully working

--- src/app/lib/middleware.ts
+++ src/app/lib/middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(req: NextRequest) {
     } = await supabase.auth.getSession();
 
     // Allow requests to the login page without a session
-    const isLoginPage = req.nextUrl.pathname === "/login";
+    const isLoginPage = req.nextUrl.pathname === "/";
 
     // Log session for debugging
     console.log("Session data:", session);
